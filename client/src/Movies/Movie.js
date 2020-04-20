875e877c0b1b966dd44a9705f25490266ba60a78
Angelg10 <angelguzman97@gmail.com>
2020-04-20 16:36:02 -0400

    part 1, some of part2

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams, useHistory } from 'react-router-dom'
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+
+  const { id } = useParams()
+
+  const history = useHistory()
+
+  // console.log(history, 'useHistory goooooooooodies')
+  // console.log(id, 'id from URL PARAMATER')
+
   useEffect(() => {
-    const id = 1;
+    
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -18,8 +26,8 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
-  
+  },[id]);
+
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
